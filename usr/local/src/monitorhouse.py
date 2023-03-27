d2e64ca24cc2e2250208d24eab4a7ad8df69a12b
Geocoronium Laboratory <57933687+geocoronium@users.noreply.github.com>
2023-03-27 18:59:12 +0900

    Update monitorhouse.py
    Remove constants to a new file.

--- usr/local/src/monitorhouse.py
+++ usr/local/src/monitorhouse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import constants
 import serial
 import serial.tools.list_ports
 import confuse
@@ -13,26 +14,6 @@ from socket import gethostname
 from hashlib import md5
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import Queue
-
-# Global constant definitions
-CFG_AMBIENT = "ambient"
-CFG_SENSORS = "sensors"
-CFG_SENSORS_NAME = "name"
-CFG_SENSORS_VID = "vid"
-CFG_SENSORS_PID = "pid"
-CFG_SENSORS_SPECS = "specs"
-CFG_SENSORS_SPECS_TYPE = "type"
-CFG_SENSORS_SPECS_FACTOR = "factor"
-CFG_SENSORS_SPECS_REGEXP = "regexp"
-CFG_SENSORS_SPECS_NEWLINE = "newline"
-CFG_SENSORS_SPECS_TYPE_ON_DEMAND = "on_demand"
-CFG_SENSORS_SPECS_COMMAND = "command"
-CFG_SENSORS_SPECS_GET_INTERVAL = "get_interval"
-CFG_SENSORS_SPECS_SAMPLING_TIMES = "sampling_times"
-CFG_SENSORS_SPECS_TYPE_AUTONOMOUS = "autonomous"
-CFG_SENSORS_SPECS_STARTCOMMAND = "startcommand"
-CFG_SENSORS_SPECS_STOPCOMMAND = "stopcommand"
-CFG_SENSORS_SPECS_COMMAND_RESULT = "command_result"
 
 loop=True
 
